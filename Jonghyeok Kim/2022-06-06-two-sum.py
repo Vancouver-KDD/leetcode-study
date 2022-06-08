a71c707167d943b39be94891a54f97d56af57073
Jonghyeok K <jka236@sfu.ca>
2022-06-07 17:26:22 -0700

    two-sum

---
+++ Jonghyeok Kim/2022-06-06-two-sum.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        for index in range(len(nums)):
+            diff = target - nums[index]
+            if diff in hash_map:
+                return [index, hash_map[diff]]
+            else:
+                hash_map[nums[index]] = index
