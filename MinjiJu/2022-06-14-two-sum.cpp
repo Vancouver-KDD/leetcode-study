b963b8cb6862439d69a1baf2cfaa4998ccd4628e
minji <minji_ju@sfu.ca>
2022-06-14 22:54:10 -0700

    initial brute force approach

---
+++ MinjiJu/2022-06-14-two-sum.cpp
@@ -0,0 +1,19 @@
+// brute force approach
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<int> res;
+        
+        for(int i=0; i<=nums.size(); i++){
+            for(int j=i+1; j<=nums.size()-1; j++){
+                if(nums[i]+nums[j]==target){
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+};
