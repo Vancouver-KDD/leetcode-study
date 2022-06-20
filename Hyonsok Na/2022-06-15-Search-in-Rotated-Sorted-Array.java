40700e8dbf7faaf312f6df9b937fd4e517e52c4e
Hyonsok <46020558+Hyonsok@users.noreply.github.com>
2022-06-19 21:10:22 -0700

    updated some lines

--- Hyonsok Na/2022-06-15-Search-in-Rotated-Sorted-Array.java
+++ Hyonsok Na/2022-06-15-Search-in-Rotated-Sorted-Array.java
@@ -1,59 +1,38 @@
-// couldn't solve
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length != 0) {
-        if(target < nums[0]) {
-            for(int i = nums.length/2+1; i<nums.length; i++) {
-                if(nums[i] == target) {
-                    return i;
-                } else {
-                    return -1;
-                }
-            }
-        } else {
-            for(int i = 0; i<=nums.length/2; i++) {
-                if(nums[i] == target) {
-                    return i;
-                } else {
-                    return -1;
-                }
-            }
+        if (nums == null || nums.length < 1) {
+            return -1;
         }
-        }
-        return -1;
         
+        int mid, left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+            mid = left + ((right - left) / 2);
+            
+            // if found, return mid -- index!!
+            if (nums[mid] == target) return mid;
+            
+            // one side will alwasy be sorted
+            if (nums[mid] >= nums[left]) {
+                // is target on left?
+                if (target >= nums[left] && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // is target on right?
+                if (target <= nums[right] && target >= nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        
+        // not found
+        return -1;
     }
 }
 
-//from reference
-class Solution {
-    // in rotated sorted array atleast one half will be sorted.
-    // Apply binary search in that half
-    public int search(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length-1;
-        
-        while (l <= r) {
-            int mid = l + (r-l)/2;
-        
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[l] <= nums[mid]) {
-                // means first half is sorted
-                if (target >= nums[l] && target < nums[mid]) {
-                    r = mid-1;
-                } else {
-                    l = mid+1;
-                }
-            } else {
-                // means 2nd half is sorted
-                if (target > nums[mid] && target <= nums[r]) {
-                    l = mid+1;
-                } else {
-                    r = mid-1;
-                }
-            }
-        }
-        return -1;
-    }
-}
--- Hyonsok Na/2022-06-17-Find-Minimum-Rotated-Sorted-Array.java
+++ Hyonsok Na/2022-06-17-Find-Minimum-Rotated-Sorted-Array.java
@@ -1,46 +1,21 @@
-// couldn't solve
 class Solution {
     public int findMin(int[] nums) {
-        int r=nums.length/2;
-        int l = r-1;
-        int count=1;
-        int output = 0;
+        int l = 0;
+        int r = nums.length-1;
         if(nums.length==1) return nums[0];
-        if(nums[0]<nums[nums.length-1]) return nums[0];
-        while(l<r) {
-            if(nums[l]<nums[r]) {
-                count++;
-                r += nums.length/Math.pow(2,count);
-                l = r-1;
-            } else {
-                output = nums[r];
-                l=r+1;
-            }
-        }
-        return output;
-    }
-}
-
-//reference
-class Solution {
-    public int findMin(int[] nums) {
-        int start=0,end=nums.length-1;
-        int mid=0;
-        if(nums[0]<=nums[end]){
-            return nums[0];
-        }
-        while(start<=end){
-            mid=start+(end-start)/2;
-            if(mid < end && nums[mid]>nums[mid+1]) {
+        if(nums[0]<nums[r]) return nums[0];
+        while(l<=r) {
+            int mid = l+(r-l)/2;
+            if(nums[mid]>nums[mid+1]) {
                 return nums[mid+1];
             }
-            if(mid>start && nums[mid-1]>nums[mid]){
+            if(nums[mid]<nums[mid-1]) {
                 return nums[mid];
             }
-            if(nums[start]<nums[mid]){
-                start=mid+1;
+            if(nums[mid]>nums[0]) {
+                l=mid+1;
             } else {
-                end=mid-1;
+                r=mid-1;
             }
         }
         return -1;
