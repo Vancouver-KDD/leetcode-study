237e36790039010fbbe7a813337aee32ce21a6b8
HS940405 <hansol.yang.94@gmail.com>
2022-06-19 18:53:09 -0700

    week 2 exercises

---
+++ HansolYang/3sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+
+            if (i > 0 and a == nums[i - 1]):
+                continue
+
+            l = i + 1
+            r = len(nums) - 1
+
+            while (l < r):
+                tsum = a + nums[l] + nums[r]
+                if tsum > 0:
+                    r -= 1
+                elif tsum < 0:
+                    l += 1
+                else:
+                    result.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+
+        return result
