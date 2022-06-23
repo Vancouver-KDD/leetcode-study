1767becb06e7a07faadde800c66120976b9464cd
HyungJoon <itineris24@naver.com>
2022-06-23 01:03:06 -0700

    Count bits without using helper function

--- HyungJoonLEE/2022-06-22-Counting-Bits.c
+++ HyungJoonLEE/2022-06-22-Counting-Bits.c
@@ -1,7 +1,23 @@
 #include "common.h"
 
+
+
+int* countBits2(int n, int* returnSize) {
+    *returnSize = n + 1;
+
+    int* result = malloc(sizeof(int) * (*returnSize));
+    result[0] = 0;
+
+    for(int i = 1; i <= n; i++) {
+        result[i] = (i & 1) ? result[i >> 1] + 1 : result[i >> 1];
+    }
+    return result;
+}
+
+
 int helper(int num) {
     int count = 0;
+
     while(num != 0) {
         if ((num & 1) == 1) count ++;
         num >>= 1;
@@ -14,7 +30,7 @@ int helper(int num) {
 int* countBits(int n, int* returnSize) {
     *returnSize = n + 1;
     int *result = malloc(sizeof(int) * (*returnSize));
-    int count = 0;
+
     for (int i = 0; i <= n; i++) {
         result[i] = helper(i);
     }
