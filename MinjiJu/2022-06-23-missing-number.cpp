2e8336013878ec18b82740038d29bd3f93c1e1ca
minji <minji_ju@sfu.ca>
2022-06-23 12:01:21 -0700

    LC#268 using Gauss formula

--- MinjiJu/2022-06-23-missing-number.cpp
+++ MinjiJu/2022-06-23-missing-number.cpp
@@ -1,3 +1,20 @@
+// using Gauss' Formula
+// sum{0, 1, ..., n} = n(n+1)/2
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int sum_exp =  nums.size()*(nums.size()+1)/2;
+        int sum_act = 0;
+        
+        for (int i=0; i<nums.size(); i++) {
+            sum_act+=nums[i];
+        }
+        return sum_exp - sum_act;
+    }
+};
+
+// naive approach
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
