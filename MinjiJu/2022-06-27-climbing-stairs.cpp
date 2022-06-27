becb482501d539034bf2821309b63d1f9be0dfe1
minji <minji_ju@sfu.ca>
2022-06-27 13:34:42 -0700

    fibonacci using for loop

--- MinjiJu/2022-06-27-climbing-stairs.cpp
+++ MinjiJu/2022-06-27-climbing-stairs.cpp
@@ -1,3 +1,23 @@
+// fibonacci using for loop
+class Solution {
+public:
+    int climbStairs(int n) {
+        // number of methods depicts fibonacci
+        // {1,2,3,5,8,13,...}
+        
+        if(n<0) return 0;
+        
+        int methods[n+1];
+        methods[0]=1, methods[1]=1;
+        
+        for(int i=2; i<=n; i++){
+            methods[i] = methods[i-1] + methods[i-2];
+        }
+        return methods[n];
+    }
+};
+
+// fibonacci using recursion
 // time limit exceeded
 class Solution {
 public:
