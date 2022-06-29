77bca6096b4bd4313d7300757af4c7b0e6e0f9aa
Hyerin <hyerin.bcit@gmail.com>
2022-06-28 18:54:43 -0700

    fixed the error

--- Hyerin Cheon/2022-06-28-Coin-Change.js
+++ Hyerin Cheon/2022-06-28-Coin-Change.js
@@ -1,10 +1,10 @@
 function coinChange(coins, amount){
-  dp = new Array(amount + 1).fill(Infinity);
+  const dp = new Array(amount + 1).fill(Infinity);
   dp[0] = 0;
 
   for(let coin of coins) {
     for(let i = coin; i <= amount; i++){
-      dp[i] = Math.min(dp[i], dp[i - coin]);
+      dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
     }
   }
   return dp[amount] === Infinity ? -1 : dp[amount];
