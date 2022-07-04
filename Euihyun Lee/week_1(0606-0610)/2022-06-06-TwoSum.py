a9e03c7fdc4b79992e4c8f3664f21265dd2c81cf
Jonghyeok K <jka236@sfu.ca>
2022-07-03 21:45:53 -0700

    clean up

--- Euihyun Lee/week_1(0606-0610)/2022-06-06-TwoSum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i, n in enumerate(nums):
-            m = target - n
-            if m in d:
-                return [d[m], i]
-            else:
-                d[n] = i
