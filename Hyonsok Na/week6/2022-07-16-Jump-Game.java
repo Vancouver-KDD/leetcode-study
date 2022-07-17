8ab9219a5abef05976dc2e417e3b298dcfe3baec
Hyonsok <46020558+Hyonsok@users.noreply.github.com>
2022-07-17 07:22:07 -0700

    week6 solutions

---
+++ Hyonsok Na/week6/2022-07-16-Jump-Game.java
@@ -0,0 +1,8 @@
+public boolean canJump(int[] A) {
+    int max = 0;
+    for(int i=0;i<A.length;i++){
+        if(i>max) {return false;}
+        max = Math.max(A[i]+i,max);
+    }
+    return true;
+}
