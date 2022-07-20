8ef96f5c8966fd9a5dc9914f24ceed84cd115444
Hyerin Cheon <72121057+Hyerin22@users.noreply.github.com>
2022-07-20 15:39:03 -0700

    Delete Jonghyeok Kim directory

--- Jonghyeok Kim/2022-06-07-best-time-to-buy-and-sell-stock.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        min_num, max_profit = prices[0], 0
-        for idx, num in enumerate(prices):
-            min_num = min(min_num, num)
-            max_profit = max(num-min_num, max_profit)
-        return max_profit
