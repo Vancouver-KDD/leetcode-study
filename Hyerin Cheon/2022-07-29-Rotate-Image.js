734fed529e9a70447392808b22cb9e276c6ef20f
Hyerin <hyerin.bcit@gmail.com>
2022-08-02 23:16:43 -0700

    fixed the comment

--- Hyerin Cheon/2022-07-29-Rotate-Image.js
+++ Hyerin Cheon/2022-07-29-Rotate-Image.js
@@ -6,9 +6,9 @@
 function rotate(matrix){
   for(let i = 0; i < matrix.length; i++){
     for(let j = i + 1; j < matrix[0].length; j++){
-      temp = matrix[j][i];  //[1][0] 2
-      matrix[j][i] = matrix[i][j]; // 2 --> 4
-      matrix[i][j] = temp;  // 4 --> 2
+      temp = matrix[j][i];  //[1][0] 4
+      matrix[j][i] = matrix[i][j]; // 4 --> 2
+      matrix[i][j] = temp;  // 2 --> 4
     }
   }
 
