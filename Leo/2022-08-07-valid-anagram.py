c7eabeed215619a67a697d4e8240a347304b52f8
DonRyu <alpharyu1031@gmail.com>
2022-09-11 22:46:22 -0700

    initial commit

--- Leo/2022-08-07-valid-anagram.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        sortedS = sorted(s)
-        sortedT = sorted(t)
-
-        if sortedS == sortedT:
-            return True
