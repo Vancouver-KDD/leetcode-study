eb2ff744ebd05e46eb49a2433d5be0ba1a40e755
sungcalgary <39603775+sungcalgary@users.noreply.github.com>
2022-09-13 22:47:18 -0700

    Update 2022-09-13-Best-Time-to-Buy-and-Sell-Stock.py

--- Sunghyun Lee/2022-09-13-Best-Time-to-Buy-and-Sell-Stock.py
+++ Sunghyun Lee/2022-09-13-Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,8 +1,8 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        max = 0;
-        for i in range(0,len(prices)):
-            for j in range(i+1,len(prices)):
-                if(prices[j] - prices[i] > max):
-                    max = prices[j] - prices[i]
-        return max
+        maxresult = 0;
+        buyingPrice= prices[0]
+        for i in range(1,len(prices)):
+            maxresult = max(maxresult, prices[i]-buyingPrice)
+            buyingPrice = min(buyingPrice, prices[i])
+        return maxresult
