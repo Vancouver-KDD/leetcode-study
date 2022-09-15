87229cc26b0924274bdd56862c6331210e86147c
Damon-Jo <lunaticjky@gmail.com>
2022-09-14 23:52:36 -0700

    added contains duplication question

---
+++ KwanyongJo/2022-09-14-Contains-Duplicate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+
+        return False
