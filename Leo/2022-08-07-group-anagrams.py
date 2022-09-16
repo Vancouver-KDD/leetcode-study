a10366b8cb2dd63c79688ec7fc787782a20d98c5
Jonghyeok K <jka236@sfu.ca>
2022-09-15 23:00:53 -0700

    clean repo

--- Leo/2022-08-07-group-anagrams.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = collections.defaultdict(list)
-        for s in strs:
-            groups[tuple(sorted(s))].append(s)
-        return map(sorted, groups.values())
