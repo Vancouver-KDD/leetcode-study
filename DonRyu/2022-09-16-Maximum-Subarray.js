e2ed21868f7f88eec21da69582d9580f837acb94
DonRyu <alpharyu1031@gmail.com>
2022-09-20 20:02:24 -0700

    max-pro-sub

--- DonRyu/2022-09-16-Maximum-Subarray.js
+++ DonRyu/2022-09-16-Maximum-Subarray.js
@@ -10,17 +10,51 @@
 
 // Input: nums = [5,4,-1,7,8]
 // Output: 23
+//
+// let maxSubArray0 = (nums) => {
+//   if (nums.length === 1) return nums[0]
+//   let maxSum = 0
+//   let prefix = 0
+//
+//   for (let a = 0; a < nums.length; a++) {
+//     prefix = nums[a] + nums[a + 1]
+//     if (prefix > 0) {
+//       maxSum = prefix
+//     } else {
+//       prefix = 0
+//     }
+//   }
+//   return maxSum
+// }
 
-let maxSubArray = function (nums) {
+let maxSubArray = (nums) => {
+  if (nums.length === 1) return nums[0]
 
+  let maxValue = nums[0]
+  let accNum = nums[0]
 
-  let maxSum = nums[0];
-
-  for (let a = 1; a < nums.length; a++) {
-    nums[a] = Math.max(nums[a], nums[a] + nums[a - 1])
-    maxSum = Math.max(nums[a], maxSum)
+  for (let i = 1; i < nums.length; i++) {
+    let calc = Math.max(nums[i], accNum + nums[i])
+    if(calc >maxValue ) {
+        maxValue = calc
+    }
+    accNum = calc
   }
-  return maxSum
-};
+  return maxValue
 
-console.log(maxSubArray([-2, -1]))
+}
+
+
+// let maxSubArray = function (nums) {
+//   let maxSum = nums[0];
+//
+//   for (let a = 1; a < nums.length; a++) {
+//     nums[a] = Math.max(nums[a], nums[a] + nums[a - 1])
+//     maxSum = Math.max(nums[a], maxSum)
+//   }
+//   return maxSum
+// };
+
+
+console.log(answerSub([-2,-1]))
+
