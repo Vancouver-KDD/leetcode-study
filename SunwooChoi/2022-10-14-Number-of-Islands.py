ab5f800d3f5698fad8a378ce894754aba7bee5f7
Sunwoo <chsunwoo1002@gmail.com>
2022-10-19 15:36:30 -0700

    [Sunwoo] Number of Island

--- SunwooChoi/2022-10-14-Number-of-Islands.py
+++ SunwooChoi/2022-10-14-Number-of-Islands.py
@@ -7,18 +7,18 @@ class Solution:
                 # find the island
                 if grid[row][col] == "1":
                     num_island += 1
-                    self.backTrackLands(grid, row, col)
+                    self.traverseLand(grid, row, col)
         return num_island
 
     # mark all land elements of the current island
-    def backTrackLands(self, grid: List[List[str]], row: int, col:int) -> None:
+    def traverseLand(self, grid: List[List[str]], row: int, col:int) -> None:
         # check out of boundary
         if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]):
             return
         # if current element is a land, keep marking adjacent elements
         if grid[row][col] == "1":
             grid[row][col] = "2"
-            self.backTrackLands(grid, row+1, col)
-            self.backTrackLands(grid, row-1, col)
-            self.backTrackLands(grid, row, col+1)
-            self.backTrackLands(grid, row, col-1)
+            self.traverseLand(grid, row+1, col)
+            self.traverseLand(grid, row-1, col)
+            self.traverseLand(grid, row, col+1)
+            self.traverseLand(grid, row, col-1)
