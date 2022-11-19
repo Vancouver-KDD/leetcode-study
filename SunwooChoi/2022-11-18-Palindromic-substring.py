8181b1809c53995924ff181f0caa87cff09e2e7f
Sunwoo <chsunwoo1002@gmail.com>
2022-11-18 17:25:13 -0800

    [Sunwoo] Palindromic Substring

--- SunwooChoi/2022-11-18-Palindromic-substring.py
+++ SunwooChoi/2022-11-18-Palindromic-substring.py
@@ -1,21 +1,14 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         size = len(s)
-        # if there is one or zero, return itself
-        if size <= 1:
-            return size
-
         result = 0
+
         for i in range(size):
-            # get longest palidrome with one element
-            solo = self.get_longest_pali(i,i,s)
-            # get longest palidrome with two elements
-            neighor = self.get_longest_pali(i,i+1,s)
-            result += solo
-            result += neighor
+            result += self.get_num_palindromes(i,i,s)
+            result += self.get_num_palindromes(i,i+1,s)
         return result
 
-    def get_longest_pali(self, left: int, right: int, s: str) -> int:
+    def get_num_palindromes(self, left: int, right: int, s: str) -> int:
         result = 0
         while left >= 0 and right < len(s) and s[left] == s[right]:
             result += 1
