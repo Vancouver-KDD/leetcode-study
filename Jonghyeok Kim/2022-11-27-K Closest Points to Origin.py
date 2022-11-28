0cb500246c453ac195db3ae4109633be728df95d
Jonghyeok K <jka236@sfu.ca>
2022-11-27 19:36:04 -0800

    [Jonghyeok Kim] Task Scheduler

---
+++ Jonghyeok Kim/2022-11-27-K Closest Points to Origin.py
@@ -0,0 +1,15 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        pts = []
+        for x, y in points:
+            dist = (abs(x - 0) ** 2) + (abs(y - 0) ** 2)
+            pts.append([dist, x, y])
+
+        res = []
+        
+        # heapd.heapify on list of list uses the first element in the child list
+        heapq.heapify(pts)
+        for i in range(k):
+            dist, x, y = heapq.heappop(pts)
+            res.append([x, y])
+        return res
---
+++ Jonghyeok Kim/2022-11-27-Kth Largest Element In An Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findKthLargest(self, nums, k):
+        if not nums: return
+        pivot = random.choice(nums)
+        left =  [x for x in nums if x > pivot]
+        mid  =  [x for x in nums if x == pivot]
+        right = [x for x in nums if x < pivot]
+        
+        L, M = len(left), len(mid)
+        
+        if k <= L:
+            return self.findKthLargest(left, k)
+        elif k > L + M:
+            return self.findKthLargest(right, k - L - M)
+        else:
+            return mid[0]
