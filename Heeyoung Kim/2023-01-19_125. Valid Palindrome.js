0832fdce1cb1b0f784531fcfb60f8b2eaa51e0d8
kimmy <hiyoung.kimmy@gmail.com>
2023-01-19 13:58:48 -0800

    🔥 01-19  Blind 75 Day 4

--- Heeyoung Kim/2023-01-19_125. Valid Palindrome.js
+++ Heeyoung Kim/2023-01-19_125. Valid Palindrome.js
@@ -1,8 +1,5 @@
 // A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
-
 // Given a string s, return true if it is a palindrome, or false otherwise.
-
- 
 
 // Example 1:
 
@@ -20,3 +17,23 @@
 // Output: true
 // Explanation: s is an empty string "" after removing non-alphanumeric characters.
 // Since an empty string reads the same forward and backward, it is a palindrome.
+
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[\W_]/g, "");
+     let left = 0;
+     let right = s.length-1;
+   
+   while(left<right){
+     if(s[left] === s[right]) {
+       left ++;
+       right--;
+     }else{
+        return false;
+    }
+   }
+   return true;
+ }
+
+ //Time Complexity : O(logN) Just check half of the array
+ //Space Complexity : O(1) No extra space needed
+ 
