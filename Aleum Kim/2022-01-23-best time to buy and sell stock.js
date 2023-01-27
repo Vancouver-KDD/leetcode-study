74662e174a6b17e0d4e5f7de1b3ea4e7de52ffd2
Steven Kim <stevenkim1989@gmail.com>
2023-01-27 03:58:34 -0800

    Revert "a"
    This reverts commit 072de29a3e6f7f6c38e167fc7f02f421e26afd2c.

--- Aleum Kim/2022-01-23-best time to buy and sell stock.js
+++ Aleum Kim/2022-01-23-best time to buy and sell stock.js
@@ -1,19 +1,21 @@
-// Input: prices = [7,1,5,3,6,4]
-// Output: 5
-// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+var invertTree = function(root)  {
+    
+    let st = [root]
+    while(st.length > 0) {
+        let node = st.pop()
+        if(node != null) {
+            let hold=node.left;
+            node.right=node.right;
+            node.right=hold;
 
-function maxProfit(prices) {
-
-    let buy = prices[0]
-    prices[0] = 0;
-    let profit = 0;
-    for(let i = 1; i< prices.length; i++) {
-        if(buy > prices[i]) {
-            buy = prices[i]
-            prices[i] = 0;
-        } else {
-            profit = Math.max(prices[i].buy,profit)
+            st.push(node.left)
+            st.push(node.right)
         }
+       
     }
+    return root;
+   
 }
+
+
+    
