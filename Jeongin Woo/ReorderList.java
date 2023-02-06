7f2424d4e173f3f3be53df42c88ce1dcfd815ffd
niniwoo <qhdk6914@naver.com>
2023-02-05 19:12:59 -0800

    Jeongin Woo Week 4

---
+++ Jeongin Woo/ReorderList.java
@@ -0,0 +1,47 @@
+
+
+public class ReorderList {
+	// check again 
+	  public class ListNode {
+		     int val;
+		     ListNode next;
+		     ListNode() {}
+		      ListNode(int val) { this.val = val; }
+		      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+		  }
+	  
+	    public void reorderList(ListNode head) {
+        if(head==null||head.next==null) return;
+            
+        
+            ListNode p1=head;
+            ListNode p2=head;
+            while(p2.next!=null&&p2.next.next!=null){ 
+                p1=p1.next;
+                p2=p2.next.next;
+            }
+            
+          
+            ListNode preMiddle=p1;
+            ListNode preCurrent=p1.next;
+            while(preCurrent.next!=null){
+                ListNode current=preCurrent.next;
+                preCurrent.next=current.next;
+                current.next=preMiddle.next;
+                preMiddle.next=current;
+            }
+            
+           
+            p1=head;
+            p2=preMiddle.next;
+            while(p1!=preMiddle){
+                preMiddle.next=p2.next;
+                p2.next=p1.next;
+                p1.next=p2;
+                p1=p2.next;
+                p2=preMiddle.next;
+            }
+
+	    }
+
+}
