0ad3fba2d72f51a30f1a3f8f0147a203fa452363
Jaydon Lee <hjjaydon.lee@gmail.com>
2023-02-07 18:30:34 -0800

    [Hyukjoo Lee] Linked List Cycle

--- HyukjooLee/2023-02-07-Linked List Cycle.java
+++ HyukjooLee/2023-02-07-Linked List Cycle.java
@@ -6,6 +6,9 @@
 // loop the list until fast != slow
 // and if fast pointer is null or there is no node linked with, it means linked list is not cycled
 // time complexity is O(N) as we traverse the head node; the length of the linked list 
+// space complexity is O(1), as it does not create any additional data structures; e.g. hashset
+// only need slow and fast pointers
+// memory used remains constant; not rely on the size of linkedlist
 public class Solution {
     public boolean hasCycle(ListNode head) {
         // slow and fast pointers
@@ -31,6 +34,8 @@ public class Solution {
 // main purpose is to store each node's reference(hash), not key or value
 // you encounter a node that is already in the set,
 // then you know there is a cycle in the list
+// time complexity is O(n) - to traverse every nodes of list when there is no cycle or all nodes are distinct
+// space complexity is O(n) - to store each reference of node, we created hashset
 public class Solution {
     public boolean hasCycle(ListNode head) {
         HashSet<ListNode> set = new HashSet<>();
