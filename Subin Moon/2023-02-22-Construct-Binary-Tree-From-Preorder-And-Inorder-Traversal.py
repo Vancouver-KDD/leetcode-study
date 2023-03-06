dddd1f407837cbf81a1e890e8283cfc9b255d336
Subin <77426279+Subin-Moon-44@users.noreply.github.com>
2023-03-05 19:07:55 -0800

    Complete Week 8

--- Subin Moon/2023-02-22-Construct-Binary-Tree-From-Preorder-And-Inorder-Traversal.py
+++ Subin Moon/2023-02-22-Construct-Binary-Tree-From-Preorder-And-Inorder-Traversal.py
@@ -32,3 +32,14 @@ class Solution:
             root.left = self.buildTree(preorder, inorder[0:ind])
             root.right = self.buildTree(preorder, inorder[ind+1:])
             return root
+
+    def buildTree_2(self, preorder, inorder):
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree_2(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree_2(preorder[mid+1:], inorder[mid+1:])
+
+        return root
