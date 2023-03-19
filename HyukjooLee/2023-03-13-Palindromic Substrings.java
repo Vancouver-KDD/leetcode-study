5458932039d19883eb0f55ebc1cb6ac9f59590e4
Jaydon Lee <hjjaydon.lee@gmail.com>
2023-03-19 16:02:18 -0700

    [Hyukjoo Lee] Palindromic Substrings

--- HyukjooLee/2023-03-13-Palindromic Substrings.java
+++ HyukjooLee/2023-03-13-Palindromic Substrings.java
@@ -4,41 +4,23 @@
 
 // A substring is a contiguous sequence of characters within the string.
 
+// Input: s = "abc"
+// Output: 3
+// Explanation: Three palindromic strings: "a", "b", "c".
 
-
-public String longestPalindrome(String s) {
+public int countSubstrings(String s) {
     int n = s.length();
-    if (n < 2) {
-        return s;
-    }
+    int count = 0;
     
-    int start = 0;
-    int end = 0;
-    
-    for (int i = 0; i < n; i++) {
-        // check for odd palindrome
-        int len1 = expandAroundCenter(s, i, i);
-        // check for even palindrome
-        int len2 = expandAroundCenter(s, i, i+1);
-        int len = Math.max(len1, len2);
+    for (int center = 0; center < 2 * n - 1; center++) {
+        int left = center / 2;
+        int right = left + center % 2;
         
-        // if we found a longer palindrome, update the start and end
-        if (len > end - start) {
-            start = i - (len - 1) / 2;
-            end = i + len / 2;
+        while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+            count++;
+            left--;
+            right++;
         }
     }
-    
-    return s.substring(start, end+1);
-}
-
-// helper function to expand around a center character to find a palindrome
-// return the length of the palindrome centered
-private int expandAroundCenter(String s, int left, int right) {
-    int n = s.length();
-    while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
-        left--;
-        right++;
-    }
-    return right - left - 1;
+    return count;
 }
