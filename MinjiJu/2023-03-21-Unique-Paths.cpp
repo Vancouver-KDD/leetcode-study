df47d9980f7f7ccae58d3b8349771c7c4b066aa4
minji <minji_ju@sfu.ca>
2023-03-25 12:55:06 -0700

    week 11

---
+++ MinjiJu/2023-03-21-Unique-Paths.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        double res = 1;
+        
+        for(int i=1; i<min(n,m); i++){
+            res = res*(n+m-i-1)/i;
+        }
+        return (int)res;
+    }
+};
