d9a03ad52f44b3a203b56b157c54acb460b94940
suekim59 <74885313+suekim59@users.noreply.github.com>
2023-04-09 07:14:56 -0700

    [Soojeong Kim] Jump Game

---
+++ Soojeong Kim/2023-04-09 Non-overlapping Intervals.java
@@ -0,0 +1,26 @@
+//interval scheduling
+//The interval scheduling maximization problem (ISMP) is 
+//to find a largest compatible set, i.e., a set of non-overlapping intervals of maximum size.
+//참고 : https://snupi.tistory.com/34
+//https://en.wikipedia.org/wiki/Interval_scheduling#Interval_Scheduling_Maximization
+
+import java.util.*;
+
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int result = 0;
+        //end로 비교하기
+        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[1], i2[1]));
+        int end = intervals[0][1];
+        int count = 1;
+
+        for(int i = 1; i<intervals.length;i++){
+            if(intervals[i][0]>=end) {
+                end = intervals[i][1];
+                count++;
+            }
+        }
+        return intervals.length-count;
+    }
+}
+//Tc : O(NlogN)
