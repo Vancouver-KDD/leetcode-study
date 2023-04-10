19af5f164c26c1388e89246b31b8efac26d54d51
minji <83440142+minji-mj@users.noreply.github.com>
2023-04-09 17:43:48 -0700

    final week

---
+++ MinjiJu/2023-04-07-Counting-Bits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res;
+        
+        for(int i=0; i<=n; i++){
+            int count = 0;  // count number of 1s
+            int val = i;    // current value
+            
+            while(val!=0){
+                count+=val%2;
+                val/=2;
+            }
+            res.push_back(count);
+        }
+        return res;
+    }
+};
