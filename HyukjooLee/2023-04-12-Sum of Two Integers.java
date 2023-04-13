6be67b42866820012a456ac5c478d900613fe183
Jaydon Lee <hjjaydon.lee@gmail.com>
2023-04-12 23:39:50 -0700

    fixed typo

--- HyukjooLee/2023-04-12-Sum of Two Integers.java
+++ HyukjooLee/2023-04-12-Sum of Two Integers.java
@@ -51,7 +51,7 @@
 public class Solution {
     public int getSum(int a, int b) {
         while (b != 0) {
-            int sspace complexity ismWithoutCarry = a ^ b;
+            int sumWithoutCarry = a ^ b;
             int carry = (a & b) << 1;
             a = sumWithoutCarry;
             b = carry;
