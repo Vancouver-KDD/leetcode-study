e4801f8703c77c19f29325a4291912d38813474e
NB <nash4comp@gmail.com>
2023-05-04 12:13:43 -0700

    Finish code for 121

--- Nash_Baek/2023-05-03-Best-Time-To-Buy-And-Sell-Stock.py
+++ Nash_Baek/2023-05-03-Best-Time-To-Buy-And-Sell-Stock.py
@@ -1,4 +1,5 @@
 # For more description, please visit the blog below.
+# https://peterdrinker.tistory.com/477
 
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
@@ -8,13 +9,10 @@ class Solution:
         # Each of them requires O(N) of time complexity.
         if not 1 <= len(prices) <= 10**5:
             raise ValueError("The length of prices should be from 1 to 10^5.")
-            # return 0
         
         for element in prices:
             if not 0 <= element <= 10**4:
                 raise ValueError("The boundary of prices have to be from 0 to 10^4")
-                # return 0
-
 
         # Subtract index2 - index1 while moving the pointer from index0 to last index of the given list.
         # The result of subtraction will be appended into set.
@@ -26,15 +24,26 @@ class Solution:
             for element in prices:
                 if prices.index(element) != (len(prices) - 1):
                     set_list.add(prices[-1] - element)
-            prices.pop(-1)
-        # print(set_list)
+            del prices[-1]
         
         if len(set_list) == 0:
             return 0
 
         return max(set_list) if max(set_list) >= 0 else 0
     
+# Enhanced solution
+#     
+# class Solution:
+#     def maxProfit(self, prices: list[int]) -> int:
+#         res = 0
+        
+#         lowest = prices[0]
+#         for price in prices:
+#             if price < lowest:
+#                 lowest = price
+#             res = max(res, price - lowest)
+#         return res
 
-solution = Solution()
-lst = [7,1,5,3,6,4]
-print(solution.maxProfit(lst))
+# solution = Solution()
+# lst = [7,1,5,3,6,4]
+# print(solution.maxProfit(lst))
