02bc4e02b0233149133060c815404852d6ef3a6a
DaveM <56316476+min8grace@users.noreply.github.com>
2023-05-04 17:19:23 -0700

    Update 2023-05-02-Valid-Anagram.cs

--- Dave Min/2023-05-02-Valid-Anagram.cs
+++ Dave Min/2023-05-02-Valid-Anagram.cs
@@ -1,5 +1,7 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length) return false;
+
         int[] sLetters = new int[26];
         int[] tLetters = new int[26];
         //To insert all letters of string s into sLetters 
@@ -9,13 +11,10 @@ public class Solution {
         for(int i=0; i< s.Length ;i++){
             int num = (int)s[i]-(int)'a';
             sLetters[num]++;
-        }
-        //To insert all letters of string t into sLetters 
-        //based on their nemerical value from the letter
-        for(int i=0; i< t.Length ;i++){
-            int num = (int)t[i]-(int)'a';
+            num = (int)t[i]-(int)'a';
             tLetters[num]++;
-        }     
+        }
+
         //To compare  sLetters with tLetters
         //If all int value(the number of each letter) are the same, then True will be returned.
         //Otherwise, False will be return once it's found.
@@ -25,3 +24,6 @@ public class Solution {
         return true;
     }
 }
+
+// Time Complexity : O(N) 
+// Space Complexity : O(1) since the length of array is constant
