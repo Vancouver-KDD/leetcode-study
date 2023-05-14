d337d00bb9ed26960baa7343ce16243edf6d6f71
chohyeonkim <chohyeonkim94@gmail.com>
2023-05-14 10:23:05 +0900

    Second week

---
+++ Chohyeon Kim Elly kim/2023-05-08-Reverse Linked List.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        prev, curr = None, head
+
+        if not curr:
+            return curr
+
+        while curr:
+            ntNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = ntNode
+            
+        return prev
