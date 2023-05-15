42b76ec7c6a4daacbc0fd191c87c97f8a39d7f04
Mave Hur <wlahd10@gmail.com>
2023-05-14 18:47:31 -0700

    [Mave Hur] Week 2

---
+++ Mave_Hur/2023-05-12-Maximum-Depth-of-Binary-Tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+
+        return res
