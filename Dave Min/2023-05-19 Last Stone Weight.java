62c3f7c8c833bae93971e4547eef881115ac9d77
DaveM <56316476+min8grace@users.noreply.github.com>
2023-05-20 10:06:35 -0700

    Update 2023-05-19 Last Stone Weight.java

--- Dave Min/2023-05-19 Last Stone Weight.java
+++ Dave Min/2023-05-19 Last Stone Weight.java
@@ -1,1 +1,16 @@
 
+public class Solution {
+    public int LastStoneWeight(int[] stones)
+    {
+        var q = new PriorityQueue<int, int>(stones.Select(x => (x, -x)));        
+        while (q.Count > 1)
+        {   
+            int a = q.Dequeue() - q.Dequeue();
+            if (a != 0) q.Enqueue(a, -a);
+        }            
+        return (q.Count == 0) ? 0 : q.Peek();
+    }
+}
+
+TC : heapify O(N) + removing N elements O(N * log N) = N log N
+SC O(N)
