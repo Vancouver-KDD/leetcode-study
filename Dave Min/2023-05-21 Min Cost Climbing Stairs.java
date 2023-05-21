9d8c4c5d956c82d7f15d6b22f56d69b6a4a84364
DaveM <56316476+min8grace@users.noreply.github.com>
2023-05-21 01:53:01 -0700

    Update 2023-05-21 Min Cost Climbing Stairs.java

--- Dave Min/2023-05-21 Min Cost Climbing Stairs.java
+++ Dave Min/2023-05-21 Min Cost Climbing Stairs.java
@@ -1,1 +1,23 @@
+public class Solution {
+    public int MinCostClimbingStairs(int[] cost) {
 
+        int[] minimumCost = new int[cost.Length + 1];
+        
+        // Start iteration from step 2, since the minimum cost of reaching
+        // step 0 and step 1 is 0
+        for (int i = 2; i < minimumCost.Length; i++) {
+            int takeOneStep = minimumCost[i - 1] + cost[i - 1];
+            int takeTwoSteps = minimumCost[i - 2] + cost[i - 2];
+            minimumCost[i] = Math.Min(takeOneStep, takeTwoSteps);
+        }
+        
+        // The final element in minimumCost refers to the top floor
+        return minimumCost[minimumCost.Length - 1];      
+    }
+}
+  
+//1 3 2 5 6 10 4
+//    1 3 3  8 9 13
+//TC: O(N)  
+//SC: O(N)
+    
