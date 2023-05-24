837db4e7896477bfec59422ae00944cb3aef3268
DaveM <56316476+min8grace@users.noreply.github.com>
2023-05-24 15:12:06 -0700

    Update 2023-05-24 Single Number.java

--- Dave Min/2023-05-24 Single Number.java
+++ Dave Min/2023-05-24 Single Number.java
@@ -1,1 +1,15 @@
 
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> hset = new HashSet<Integer>();
+
+        for(int i=0;i<nums.length;i++){
+            if(hset.contains(nums[i])) hset.remove(nums[i]);
+            else hset.add(nums[i]);
+        }
+        for(int i : hset){
+            return i;
+        }
+        return 0;
+    }
+}
