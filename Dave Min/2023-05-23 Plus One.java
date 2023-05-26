16297c15561a2f0ad2d7a0d88196c4e4b1c4c0d2
DaveM <56316476+min8grace@users.noreply.github.com>
2023-05-25 23:39:56 -0700

    Update 2023-05-23 Plus One.java

--- Dave Min/2023-05-23 Plus One.java
+++ Dave Min/2023-05-23 Plus One.java
@@ -1,1 +1,24 @@
 
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int Carry =1;
+        int allNineChecker = 0;
+        for(int i= digits.length -1; i>= 0; i--){
+            int sum = Carry + digits[i];
+            if(sum==10){
+                digits[i] =0;
+                Carry =1;                
+            }else{
+                digits[i] =sum;
+                Carry =0;
+            }
+            allNineChecker += digits[i];
+        }
+        if(allNineChecker==0 && Carry ==1)
+        {
+          digits = new int[digits.length+1];
+          digits[0]=1;
+        }
+        return digits; 
+    }
+}
