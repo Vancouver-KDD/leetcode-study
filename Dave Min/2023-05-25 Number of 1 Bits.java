87c3461f507a29d3e56f5af55ffdc8ed132ca0c4
DaveM <56316476+min8grace@users.noreply.github.com>
2023-05-25 22:04:27 -0700

    Update 2023-05-25 Number of 1 Bits.java

--- Dave Min/2023-05-25 Number of 1 Bits.java
+++ Dave Min/2023-05-25 Number of 1 Bits.java
@@ -1,1 +1,36 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        //solution 1
+        //integer is 4 byte -> 32 bit
+        int cnt=0;
+        int comparer =1;
+        for(int i=0;i<32;i++){
+            System.out.println(n & comparer);
+            if((n & comparer) != 0) cnt++;
+            comparer <<= 1;
+        }
+        return cnt;
+        //TC O(1);
+        //SC O(1);
+
+        //solution2
+        //..1110100 -> n
+        //..1110011 -> n-1
+        //so when n & n-1 the least significant bit 1 can be removed.
+        //..1110000
+        //..1101111
+        // until n ==0
+
+        // int cnt = 0;
+        // while(n!=0){
+        //     cnt++;
+        //     n = n & (n-1);
+        // }
+
+        // return cnt;
+        //TC O(1);
+        //SC O(1);
+    }
+}
 
