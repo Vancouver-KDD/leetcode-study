b8e8f131268346fe9c1e368683a8fc3bb5309df1
kyungmin <mmkim725@student.ubc.ca>
2023-05-28 10:17:49 -0700

    week4

---
+++ Kyungmin Kim/reverseBits.java
@@ -0,0 +1,11 @@
+public class Solution{
+    public int reverseBits(int n){
+        int ans = 0;
+        for (int i = 0; i <32; i++){
+            ans <<=1;
+            ans |= (n &1);
+            n>>=1;
+        }
+        return ans;
+    }
+}
