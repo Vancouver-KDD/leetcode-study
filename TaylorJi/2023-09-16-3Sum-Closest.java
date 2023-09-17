6afa4951cc5cf5da4166ef7efd78e2e936091591
TaylorJi <sji24@my.bcit.ca>
2023-09-16 21:55:17 -0700

    removed unnecessary part from 3Sum solution

--- TaylorJi/2023-09-16-3Sum-Closest.java
+++ TaylorJi/2023-09-16-3Sum-Closest.java
@@ -36,43 +36,6 @@ public class theeSumClosest {
     }
 
     return closestSum;
-
-
-//    int threeNumbs = 3;
-//    int gap;
-//    int closeToTarget = 0;
-//    if (nums.length == 3) {
-//      return nums[0] + nums[1] + nums[2];
-//    }
-//
-//    // the largest number index to fix
-//    int holdIndex = nums.length - threeNumbs - 1;
-//    HashMap<Integer, Integer> threeSumsResult = new HashMap<>();
-//
-//    for (int i = holdIndex; i < holdIndex + 1; i++) {
-//      //adjust the for loop, this will cause outOfBound exception
-//      for (int j = i + 1; j < nums.length - 1; j++) {
-//        int temp3Sum = 0;
-//        int[] threeNumbsArr = new int [threeNumbs];
-//        threeNumbsArr[0] = nums[i];
-//        threeNumbsArr[1] = nums[j];
-//        threeNumbsArr[2] = nums[j+1];
-//        temp3Sum = nums[i] + nums[j] + nums[j+1];
-//        gap = Math.abs(Math.abs(temp3Sum) - Math.abs(target));
-//        threeSumsResult.put(temp3Sum, gap);
-//        //reset the 3numbsArr
-//        threeNumbsArr = null;
-//      }
-//    }
-//
-//    int smallestGap = Collections.min(threeSumsResult.values());
-//    for (int key : threeSumsResult.keySet()) {
-//      if (threeSumsResult.get(key) == smallestGap) {
-//        closeToTarget = key;
-//      }
-//    }
-//
-//    return closeToTarget;
   }
 
 
