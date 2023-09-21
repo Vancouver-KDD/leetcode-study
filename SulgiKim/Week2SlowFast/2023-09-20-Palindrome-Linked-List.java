26cf763dc7b212c378464737f75d6d1151034c74
sulgikim <92609054+sulgikim@users.noreply.github.com>
2023-09-21 09:00:08 -0700

    Q# Reorder List

--- SulgiKim/Week2SlowFast/2023-09-20-Palindrome-Linked-List.java
+++ SulgiKim/Week2SlowFast/2023-09-20-Palindrome-Linked-List.java
@@ -1,6 +1,5 @@
-
-
-/*
+/* https://leetcode.com/problems/palindrome-linked-list/description/
+ * 
  * ## Description 
  * Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
  * 
