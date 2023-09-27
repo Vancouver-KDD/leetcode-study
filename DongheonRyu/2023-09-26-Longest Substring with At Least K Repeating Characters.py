2490eee57bb96546257171a0a9c83473ad5502fb
don <donryu1031@gmail.com>
2023-09-27 15:52:13 -0700

    LONGsUB K

--- DongheonRyu/2023-09-26-Longest Substring with At Least K Repeating Characters.py
+++ DongheonRyu/2023-09-26-Longest Substring with At Least K Repeating Characters.py
@@ -15,15 +15,20 @@ def longestSubstring(self, s: str, k: int) -> int:
 
 
 
-def longestSubstring(self, s: str, k: int) -> int:
+def longestSubstring2(self, s: str, k: int) -> int:
             
-        max_length = 0
-    
-        for char in set(s):
-            if s.count(char) >= k:
-                substrings = s.split(char)
-                for substring in substrings:
-                    if len(substring) >= k:
-                        max_length = max(max_length, len(substring))
-    
-        return max_length
+        if len(s) < k:
+            return 0
+
+        char_count = {}
+        for char in s:
+            char_count[char] = char_count.get(char, 0) + 1
+
+        for char in s:
+            if char_count[char] < k:
+                max_length = 0
+                for substring in s.split(char):
+                    max_length = max(max_length, self.longestSubstring(substring, k))
+                return max_length
+
+        return len(s)
