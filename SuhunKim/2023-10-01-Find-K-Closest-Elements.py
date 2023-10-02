b16c6179fa3499a708dfee6d9d6fe7e8b1cf8a66
Suhun Kim <kksshh404@gmail.com>
2023-10-01 17:20:26 -0700

    code update

--- SuhunKim/2023-10-01-Find-K-Closest-Elements.py
+++ SuhunKim/2023-10-01-Find-K-Closest-Elements.py
@@ -3,18 +3,15 @@ class Solution:
         ### method 1
         # arr.sort(key=lambda i:abs(i-x))
         # return sorted(arr[:k])
-
-        # ### method 2 (heap)
-        # arr.sort(key=lambda i:abs(i-x))
-        # heap = []
-        # for i in range(k):
-        #     heapq.heappush(heap, arr[i])
-        # return sorted(heap)
+        
+        ### method 2 (heap)
+#         arr.sort(key=lambda i:abs(i-x))
+#         heap = []
+#         for i in range(k):
+#             heapq.heappush(heap, arr[i])
+#         return sorted(heap)
     
         ### method 3 (binary search)
-        # find the closest element to x
-        # find the left and right bound
-        # return the elements between the left and right bound
         left, right = 0, len(arr)-1
         while left < right:
             mid = (left+right)//2
@@ -23,12 +20,25 @@ class Solution:
             else:
                 right = mid
 
-        if left == 0:
+        left -= 1
+        if left == -1:
             return arr[:k]
         elif left == len(arr)-1:
             return arr[-k:]
         else:
-            if abs(arr[left-1]-x) <= abs(arr[right+1]-x):
-                return arr[left-k:left]
-            else:
-                return arr[right+1:right+k+1]
+            res = []
+            while len(res) < k:
+                if left == -1:
+                    res.append(arr[right])
+                    right += 1
+                elif right == len(arr):
+                    res.append(arr[left])
+                    left -= 1
+                else:
+                    if abs(arr[left]-x) <= abs(arr[right]-x):
+                        res.append(arr[left])
+                        left -= 1
+                    else:
+                        res.append(arr[right])
+                        right += 1
+            return sorted(res)
