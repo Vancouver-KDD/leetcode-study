2b931170572e60dd636f74317c1443666b91b10f
don <donryu1031@gmail.com>
2023-10-11 13:27:40 -0700

    add recursion

--- DongheonRyu/2023-10-09-Reverse Linked List.py
+++ DongheonRyu/2023-10-09-Reverse Linked List.py
@@ -9,13 +9,17 @@ def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
             head = next_node
         return prev
 
+
+# 1 -> 2 -> 3 -> 4
+#           
+
 def reverseListRecursion(head):
       if not head:
             return None
       
-      newHead = head
-      if head.next:
-            newHead = reverseListRecursion(head.next)
+      newHead = head # 4
+      if head.next: # 4
+            newHead = reverseListRecursion(head.next) #4
             head.next.next = head
       head.next = None
 
