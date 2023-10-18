503a0578673ce7daa625eda761dfcb6e53e1692e
LeeJay0126 <topaz9889@gmail.com>
2023-10-17 19:40:15 -0700

    Create 2023-10-17-Populating-Next-Right-Pointer.js

---
+++ SeungYeonLee/Week6/2023-10-17-Populating-Next-Right-Pointer.js
@@ -0,0 +1,48 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    
+    if(root == null){
+        return null;
+    }
+
+    let q = new Queue();
+    q.push(root);
+    q.push(null);
+
+    while(!q.isEmpty()){
+        let front = q.front();
+        q.pop();
+
+        if(front == null){
+            if(!q.isEmpty()){
+                q.push(null);
+            }
+        }else{
+            front.next = q.front();
+            if(front.left){
+                if(front.left){
+                    q.push(front.left);
+                    q.push(front.right);
+                }
+            }
+        }
+    }
+
+    return root;
+
+
+
+};
