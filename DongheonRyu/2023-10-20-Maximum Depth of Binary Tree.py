9cbe33caeba2cb583731dcd2c539f99b657c98c8
don <donryu1031@gmail.com>
2023-10-20 13:43:00 -0700

    max depth

--- DongheonRyu/2023-10-20-Maximum Depth of Binary Tree.py
+++ DongheonRyu/2023-10-20-Maximum Depth of Binary Tree.py
@@ -5,3 +5,19 @@ def maxDepth(self, root: Optional[TreeNode]) -> int:
         
         return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
 
+
+def maxDepth(root):
+      if not root:
+            return 0
+      
+      level = 0
+      q = deque([root])
+      while q:
+            
+            for i in range(len(q)):
+                  node = q.popleft()
+                  if node.left:
+                        q.append(node.left)
+                  if node.right:
+                        q.append(node.right)
+            level += 1    
