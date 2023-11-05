5e2d562213fd09e25fcfb788ce1e9808a1eeb4dd
Suhun Kim <kksshh404@gmail.com>
2023-11-05 14:41:24 -0800

    solution update

--- SuhunKim/2023-11-05-Find-the-Winner-of-an-Array-Game.py
+++ SuhunKim/2023-11-05-Find-the-Winner-of-an-Array-Game.py
@@ -1,5 +1,6 @@
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
+        # method 1
         if k >= len(arr)*2:
             return max(arr)
         winner = {}
@@ -23,6 +24,32 @@ class Solution:
                 player1 = player2
             
             if winner[player1] == k:
+                break
+            player2 = arr.pop(0)
+        return player1
+    
+        # method 2 (updated)
+        if k >= len(arr):
+            return max(arr)
+        winner = {}
+        
+        player1 = arr.pop(0)
+        player2 = arr.pop(0)
+        
+        while True:
+            if player1 > player2:
+                if player1 not in winner:
+                    winner[player1] = 1
+                else:
+                    winner[player1] += 1
+            else:
+                if player2 not in winner:
+                    winner[player2] = 1
+                else:
+                    winner[player2] += 1
+                player1 = player2
+            
+            if winner[player1] == k or not arr:
                 break
             player2 = arr.pop(0)
         return player1
