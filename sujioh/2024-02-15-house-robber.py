f02c6813d43036d49e8a233352a3168fe2e8fdb0
Suji <sujioh5@gmail.com>
2024-02-17 21:23:30 -0800

    house robber explanation

--- sujioh/2024-02-15-house-robber.py
+++ sujioh/2024-02-15-house-robber.py
@@ -22,3 +22,19 @@ nums = [6, 3, 10,  8,  2, 10, 3,  5,  10, 5, 3]
 s = Solution()
 s.rob(nums)
 
+'''
+이 문제는 주어진 어레이에서 가장 큰 "합"을 구하는 문제.
+하지만 "인접한 수는 포함할 수 없다"는 조건이 있다. 
+
+디피 어레이를 만든다.
+디피 어레이는 그 인덱스까지의 가장 큰 합이다.
+Using this DP array we can track the maximum sum until that index.
+
+Example: 
+the array we have is [6,3,10,8,2]
+1. the dp[0] will store just arr[0], because it is the biggest number so far.add()
+2. However, dp[1] will store the max number between arr[0] and arr[1]
+3. dp[2] will store dp[0]+arr[2] OR dp[1] << and it is the core logic of this question. 
+    - we can decided whether to include "arr[i]" or not. 
+
+'''
