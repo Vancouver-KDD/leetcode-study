fbe47014811760bcace03e77e671c56593382077
Changwhi <whinada@gmail.com>
2024-02-18 19:08:43 -0800

    Week 3

---
+++ Changwhi/2024-02-18-Word-Break.py
@@ -0,0 +1,12 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [0] * (len(s) + 1)
+        dp[len(s)] = True
+
+        for i in range(len(s) - 1 , -1, -1):
+            for word in wordDict:
+                if i + len(word) <= len(s) and s[i: i+ len(word)] == word:
+                    dp[i] = dp[i + len(word)]
+                if dp[i]:
+                    break
+        return dp[0]
