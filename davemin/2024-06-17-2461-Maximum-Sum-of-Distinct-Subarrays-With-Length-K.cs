6bc155730bb147221b6feb0b057acdd016a6e01b
DaveM <56316476+min8grace@users.noreply.github.com>
2024-06-22 00:17:29 -0700

    Update 2024-06-17-2461-Maximum-Sum-of-Distinct-Subarrays-With-Length-K.cs

--- davemin/2024-06-17-2461-Maximum-Sum-of-Distinct-Subarrays-With-Length-K.cs
+++ davemin/2024-06-17-2461-Maximum-Sum-of-Distinct-Subarrays-With-Length-K.cs
@@ -1,1 +1,36 @@
 
+public class Solution {
+    public long MaximumSubarraySum(int[] nums, int k) {
+        Dictionary<int,int> dic = new Dictionary<int,int>();
+        int l=0, r =0;
+        long max=0 , sum=0;
+
+        while(r < nums.Length){
+           if(!dic.ContainsKey(nums[r]))
+                dic.Add(nums[r],0);
+            dic[nums[r]]++;
+            sum += nums[r];
+
+            while(dic.Keys.Count < (r-l+1) || dic.Keys.Count > k)
+            {                    
+                if(dic.ContainsKey(nums[l]))
+                {
+                    sum -= nums[l];
+                    dic[nums[l]]--;
+                    if(dic[nums[l]] == 0)
+                        dic.Remove(nums[l]);
+                    l++;
+                }
+            }
+
+            if(dic.Keys.Count == (r-l+1) && k == dic.Keys.Count)
+           {
+               max = Math.Max(sum,max);  
+           }
+           r++;
+        }
+        return max;
+    }
+
+
+}
