a53f6c84c36363688d5ff266164d9a60dca4e3eb
Changwhi <whinada@gmail.com>
2024-07-28 19:46:51 -0700

    week-7

---
+++ Changwhi/2024-07-28-combinations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = []
+
+        def backtrack(start, comb):
+            if len(comb) == k:
+                result.append(comb[:])
+                return
+            for i in range(start, n+1):
+                comb.append(i)
+                backtrack(i + 1, comb)
+                comb.pop()
+        backtrack(1, [])
+        return result
