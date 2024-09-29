5e667e3e457e760819418976fba76cf0e1982ebc
youn <youn@maebe.jp>
2024-09-29 15:42:26 -0700

    Add: Week1

--- YounSangHo/Week1/2023-09-16_squares_of_a_sorted_array.js
+++
@@ -1,8 +0,0 @@
-/**
- * 배열을 받아 제곱을 하여 그 값을 내림순서로 배열
- * @param {number[]} nums
- * @return {number[]}
- */
-var sortedSquares = function (nums) {
-  return nums.map((item) => item * item).sort((a, b) => a - b);
-};
