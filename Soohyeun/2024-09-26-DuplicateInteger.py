4b5daababce1548e9e292caf05bea430d152dfed
Klaiden <itineris24@naver.com>
2024-09-30 21:58:01 -0700

    week 1

--- Soohyeun/2024-09-26-DuplicateInteger.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-         set_num = set(nums)
-
-         return False if len(set_num) == len(nums) else True
--- Yunju/2024-09-28-Is-Anagram.py
+++
@@ -1,7 +0,0 @@
-# Is Anagram
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if sorted(s) == sorted(t):
-            return True
-        return False
-        
