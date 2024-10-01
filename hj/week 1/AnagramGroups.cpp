d20b0628f87a90ffeb4bd87c6828133c11462826
Klaiden <itineris24@naver.com>
2024-09-30 21:50:17 -0700

    week 1

--- hj/week 1/AnagramGroups.cpp
+++ hj/week 1/AnagramGroups.cpp
@@ -1,3 +1,19 @@
-//
-// Created by hj on 9/30/24.
-//
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> output;
+        unordered_map<string, vector<string>> um;
+
+        for (string &str : strs) {
+            string key = str;
+            sort(key.begin(), key.end());
+            um[key].push_back(str);
+        }
+
+        for (auto &val : um) {
+            output.push_back(val.second);
+        }
+
+        return output;
+    }
+};
