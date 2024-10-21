22ea83f5a99c21e8710f9168b7cc8d20380763bc
ohyeree1 <ohyeree1@gmail.com>
2024-10-20 20:16:01 -0700

    Leetcode Week 1-3

---
+++ Hye/week3/2024-10-20-reorder-linked-list.py
@@ -0,0 +1,69 @@
+"""
+Reorder Linked List
+You are given the head of a singly linked-list.
+
+The positions of a linked list of length = 7 for example, can intially be represented as:
+
+[0, 1, 2, 3, 4, 5, 6]
+
+Reorder the nodes of the linked list to be in the following order:
+
+[0, 6, 1, 5, 2, 4, 3]
+
+Notice that in the general case for a list of length = n the nodes are reordered 
+to be in the following order:
+
+[0, n-1, 1, n-2, 2, n-3, ...]
+
+You may not modify the values in the list's nodes, but instead you must reorder the nodes themselves.
+
+Example 1:
+Input: head = [2,4,6,8]
+Output: [2,8,4,6]
+
+Example 2:
+Input: head = [2,4,6,8,10]
+Output: [2,10,4,8,6]
+
+Constraints:
+
+1 <= Length of the list <= 1000.
+1 <= Node.val <= 1000
+
+"""
+
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # Find the middle node
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        prev = slow.next = None
+        # Reverse the second part
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+        
+        # Merge them alternatively
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
+
+
