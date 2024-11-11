33db04cbd04f2e57c5cc1f49363b67564364c546
Changwhi <whinada@gmail.com>
2024-11-10 19:55:45 -0800

    6week

--- Changwhi/Oct-27-2024-find-target-in-rotated-sorted-array.py
+++ Changwhi/Oct-27-2024-find-target-in-rotated-sorted-array.py
@@ -27,3 +27,66 @@ class Solution:
             else:
                 left = mid + 1
         return res
+
+
+
+    class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        similar to finding minimum value in rotated sorted array
+        in this case, you need to find index of target value
+        
+        I would say that you can implement the same way, but adds one more condition which is greater than or equal to target.
+        if mid is smaller than first element, then, 
+        (mid < first),
+            bigger smaller <-<- mid ->-> biger 
+            if target < mid
+                to the left side
+            if target > mid
+                right or left?? 
+                if target >= first 
+                    to the left
+                if target < first
+                    to the right    
+        (mid >= first),
+            smaller <-<-  mid  ->-> bigger smaller
+            if taget < mid 
+                if target < first:
+                    to the right
+                if target > first 
+                    to the left
+
+                to the left
+            if target > mid
+                to the right             
+        """
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] < nums[left]:
+                if target < nums[mid]:
+                    right = mid -1
+                elif target > nums[mid]:
+                    if target > nums[left]:
+                        right = mid - 1
+                    elif target < nums[left]:
+                        left = mid + 1
+                    else: 
+                        return left
+                else:
+                    return mid
+            else:
+                if target < nums[mid]:
+                    if target < nums[left]:
+                        left = mid + 1
+                    elif target > nums[left]:
+                        right = mid - 1
+                    else: 
+                        return left
+                elif target > nums[mid]:
+                    left = mid + 1
+                else: 
+                    return mid
+        return -1
