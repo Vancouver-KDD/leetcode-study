9f631d93d1ec51668d29ad3a5d5e8bd746e51c00
HyungJoon <itineris24@naver.com>
2024-11-15 09:24:31 -0800

    week 7

--- hj/week7/CountConnectedComponents.cpp
+++ hj/week7/CountConnectedComponents.cpp
@@ -1,3 +1,32 @@
-//
-// Created by Hyung Joon Lee on 2024-11-15.
-//
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> graph(n);
+        int output = 0;
+
+        for (auto& edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+
+        vector<bool> visited(n, false);
+
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                output++;
+                dfs(i, graph, visited);
+            }
+        }
+
+        return output;
+    }
+
+    void dfs(int node, const vector<vector<int>>& graph, vector<bool>& visited) {
+        visited[node] = true;
+        for (int neighbor : graph[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, graph, visited);
+            }
+        }
+    }
+};
