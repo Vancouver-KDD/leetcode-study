4126f5cbe15ecd0cb1444baae68adfc25ec09140
juyomo <j.92.moon@gmail.com>
2025-06-04 19:32:11 +0900

    Add solutions to the other 2 week1 probs.

--- 2025/JuyoungMoon/week1/2025-06-08-group-anagrams.cpp
+++ 2025/JuyoungMoon/week1/2025-06-08-group-anagrams.cpp
@@ -4,3 +4,24 @@
 // https://github.com/juyomo/leetcode-study
 
 // https://leetcode.com/problems/group-anagrams/
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, int> strToIdx;
+        vector<vector<string>> res;
+        for (string s : strs) {
+            string original = s;
+            sort(s.begin(), s.end()); 
+            if (strToIdx.find(s) != strToIdx.end()) {
+                res[strToIdx[s]].push_back(original);
+            } else {
+                // Haven't seen this one before!
+                vector<string> newVec = { original };
+                strToIdx[s] = res.size();
+                res.push_back(newVec);
+            }
+        }
+        return res;
+    }
+};
--- 2025/JuyoungMoon/week1/2025-06-08-top-k-frequent-elements.cpp
+++ 2025/JuyoungMoon/week1/2025-06-08-top-k-frequent-elements.cpp
@@ -5,3 +5,39 @@
 
 // https://leetcode.com/problems/top-k-frequent-elements/
 
+class Solution {
+public:
+    static bool compare(const vector<int>& a, const vector<int>& b) {
+        // most frequent first
+        return a[0] > b[0];
+    }
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // Keep track of how many of each num is in array.
+        unordered_map<int, int> counts;
+        for (int n : nums) {
+            counts[n]++;
+        }
+
+        // Put that data into a vector.
+        vector<vector<int>> countAndNum(counts.size(), vector<int>(2));
+        int idx = 0;
+        for (const auto& p : counts) {
+            int num = p.first;
+            int count = p.second;
+            countAndNum[idx][0] = count;
+            countAndNum[idx][1] = num;
+            idx++;
+        }
+
+        // Sort from most frequent to least frequent. Meh, could do better time complexity.
+        sort(countAndNum.begin(), countAndNum.end(), compare);
+
+        // Return the top k frequent.
+        vector<int> res(k);
+        for (int i = 0; i < k; i++) {
+            res[i] = countAndNum[i][1];
+        }
+        return res;
+    }
+};
