1446b8bb86959d24fcc76b07e9293f36faecb07a
juyomo <j.92.moon@gmail.com>
2025-06-13 21:40:42 +0900

    Add file for #242. Not solved yet.

---
+++ 2025/JuyoungMoon/week2/2025-06-15-longest-repeating-character-replacement-local.cpp
@@ -0,0 +1,6 @@
+// Author: Juyoung Moon
+// KDD LeetCode Study Week 2: Two Pointer & Sliding Window.
+// https://github.com/juyomo/leetcode-study
+
+// LeetCode #424.
+// https://leetcode.com/problems/longest-repeating-character-replacement/
