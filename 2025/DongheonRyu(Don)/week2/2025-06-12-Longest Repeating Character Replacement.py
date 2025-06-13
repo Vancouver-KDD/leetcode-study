c2271e4793a78b7440f8f8dba1b7799f3b990b20
Don Ryu <alpharyu1031@gmail.com>
2025-06-12 20:49:15 -0700

    week 2

---
+++ 2025/DongheonRyu(Don)/week2/2025-06-12-Longest Repeating Character Replacement.py
@@ -0,0 +1,19 @@
+def characterReplacement(s: str, k: int) -> int:
+    from collections import defaultdict
+
+    count = defaultdict(int)
+    left = 0
+    max_freq = 0
+    result = 0
+
+    for right in range(len(s)):
+        count[s[right]] += 1
+        max_freq = max(max_freq, count[s[right]])
+
+        if (right - left + 1) - max_freq > k:
+            count[s[left]] -= 1
+            left += 1
+
+        result = max(result, right - left + 1)
+
+    return result
