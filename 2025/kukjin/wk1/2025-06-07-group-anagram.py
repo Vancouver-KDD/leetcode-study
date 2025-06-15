af8d291104694c360761c93015669c941e5fb052
kukjinkim <tlaruddo2@gmail.com>
2025-06-15 14:13:24 -0700

    Added wk2 solutions

--- 2025/kukjin/wk1/2025-06-07-group-anagram.py
+++ 2025/kukjin/wk1/2025-06-07-group-anagram.py
@@ -1,13 +1,13 @@
 
 class Solution:
-  def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-      # rst = {}
-      rst = defaultdict(list)
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # rst = {}
+        rst = defaultdict(list)
 
-      for s in strs:
-          ss = "".join(sorted(s))
-          if ss not in rst: 
-              rst[ss] = []
-          rst[ss].append(s)
+        for s in strs:
+            ss = "".join(sorted(s))
+            if ss not in rst: 
+                rst[ss] = []
+            rst[ss].append(s)
 
-      return list(rst.values())
+        return list(rst.values())
--- 2025/kukjin/wk1/2025-06-07-k-frequent-element.py
+++ 2025/kukjin/wk1/2025-06-07-k-frequent-element.py
@@ -1,14 +1,14 @@
 class Solution:
-  def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-      dic = {}
-      for n in nums: 
-          if n in dic: 
-              dic[n] += 1
-          else:
-              dic[n] = 1
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = {}
+        for n in nums: 
+            if n in dic: 
+                dic[n] += 1
+            else:
+                dic[n] = 1
 
-      # Sort by frequency (descending)
-      sorted_items = sorted(dic.items(), key=lambda item: item[1], reverse=True)
+        # Sort by frequency (descending)
+        sorted_items = sorted(dic.items(), key=lambda item: item[1], reverse=True)
 
-      # Extract the top k keys
-      return [item[0] for item in sorted_items[:k]]
+        # Extract the top k keys
+        return [item[0] for item in sorted_items[:k]]
