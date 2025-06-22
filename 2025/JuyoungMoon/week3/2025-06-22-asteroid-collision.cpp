ea14c15f0bb756864b710b41d2f37f69ddf0a452
juyomo <j.92.moon@gmail.com>
2025-06-22 23:21:28 +0900

    Fix "oops I submitted before saving" mistake :')

--- 2025/JuyoungMoon/week3/2025-06-22-asteroid-collision.cpp
+++ 2025/JuyoungMoon/week3/2025-06-22-asteroid-collision.cpp
@@ -5,36 +5,3 @@
 
 // LeetCode #735.
 // https://leetcode.com/problems/asteroid-collision/
-
-class Solution {
-public:
-    bool isValid(string s) {
-        int size = s.length();
-        if (size % 2 != 0) {
-            return false;
-        }
-        stack<char> stk;
-
-        for (int i = 0; i < size; i++) {
-            char curr = s[i];
-            switch (curr) {
-                case '[':
-                    stk.push(']');
-                    break;
-                case '{':
-                    stk.push('}');
-                    break;
-                case '(':
-                    stk.push(')');
-                    break;
-                default:
-                    if (stk.empty() || stk.top() != curr) {
-                        return false;
-                    }
-                    stk.pop();
-            }
-        }
-
-        return stk.empty();
-    }
-};
