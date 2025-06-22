dedf7d64459859958b49f30a8f3e035b5bdbe270
jnmoon <moon9461@gmail.com>
2025-06-22 16:15:15 -0700

    Add solutions for Week 3: Stack

---
+++ 2025/jiyeon/2025-06-22-asteroid-collision.java
@@ -0,0 +1,29 @@
+class AsteroidCollision {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int asteroid : asteroids) {
+            if (asteroid > 0) {
+                stack.push(asteroid);
+            } else {
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -asteroid) {
+                    stack.pop();
+                }
+
+                if (stack.isEmpty() || stack.peek() < 0) {
+                    stack.push(asteroid);
+                }
+
+                if (stack.peek() == -asteroid) {
+                    stack.pop();
+                }
+            }
+        }
+
+        int[] result = new int[stack.size()];
+        int i = stack.size() - 1;
+        while (!stack.isEmpty()) {
+            result[i--] = stack.pop();
+        }
+        return result;
+    }
+}
