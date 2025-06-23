1f27c8caf6865529cb73723b407046a50afe8dbc
Ella Lee <64251036+gyung688@users.noreply.github.com>
2025-06-22 19:42:36 -0700

    Create Create 2025-06-08-Asteroid_Collision.py

---
+++ Create 2025-06-08-Asteroid_Collision.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+        for asteroid in asteroids:
+            while stack and asteroid < 0 < stack[-1] :
+                if stack[-1] < -asteroid:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -asteroid:
+                    stack.pop()
+                break
+            else:
+                stack.append(asteroid)
+        return stack
