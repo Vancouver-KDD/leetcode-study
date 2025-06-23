e4e07bfc2a92bb914bd5214963813fdfffc12ecb
icekimchi <pjh010828@gmail.com>
2025-06-22 19:39:48 -0700

    Fix: delete wrong sentence

--- Anna Park/Week3/2025-06-22-Daily Temperatures.java
+++ Anna Park/Week3/2025-06-22-Daily Temperatures.java
@@ -1,22 +1,20 @@
 import java.util.*;
 
-class daily_temperatures {
+class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         
         int n = temperatures.length;
         Stack<Integer> stack = new Stack<>();
         int[] result = new int[n];
 
-        stack.push(temperatures[0]);
-
-        for(int i=0; i<n; i++){
+        for (int i=0; i<n; i++){
             while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
                 int prevIndex = stack.pop();
                 result[prevIndex] = i - prevIndex;
             }
             stack.push(i);
         }
-        
+
         return result;
     }
 }
