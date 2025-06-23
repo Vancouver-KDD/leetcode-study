be92f793dd0f719718414bb018a6211b5d6057b1
juyomo <juyomo@users.noreply.github.com>
2025-06-23 11:55:47 +0900

    Update 2025-06-22-daily-temperatures.cpp

--- 2025/JuyoungMoon/week3/2025-06-22-daily-temperatures.cpp
+++ 2025/JuyoungMoon/week3/2025-06-22-daily-temperatures.cpp
@@ -5,3 +5,20 @@
 
 // LeetCode #739.
 // https://leetcode.com/problems/daily-temperatures/
+
+// SOLN #1. O(n^2)
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> res(temperatures.size(), 0);
+        for (int i = 0; i < temperatures.size(); i++) {
+            for (int j = i + 1; j < temperatures.size(); j++) {
+                if (temperatures[j] > temperatures[i]) {
+                    res[i] = j - i;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
