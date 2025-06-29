560bcfe102a096e15c28b6dda1306874fe8365f4
ellieCorner <ellie25corner@gmail.com>
2025-06-29 12:45:06 -0700

    feat: Solve LeetCode Search in Rotated Sorted Array (week4)

---
+++ 2025/SoyoungJung/week4/2025-06-29-Search-in-Rotated-Sorted-Array.ts
@@ -0,0 +1,28 @@
+function searchInRotatedSortedArray(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+}
