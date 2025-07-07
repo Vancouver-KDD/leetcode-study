71c1c67647b8693b8713b4346a63af04af41bd69
juyomo <juyomo@users.noreply.github.com>
2025-07-07 11:54:17 +0900

    Update 2025-07-06-reverse-linked-list.cpp

--- 2025/JuyoungMoon/week5/2025-07-06-reverse-linked-list.cpp
+++ 2025/JuyoungMoon/week5/2025-07-06-reverse-linked-list.cpp
@@ -6,3 +6,37 @@
 // LeetCode #206.
 // https://leetcode.com/problems/reverse-linked-list/
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) {
+            return head;
+        }
+
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        ListNode* next = head->next;
+
+        while (curr != nullptr) {
+            curr->next = prev;
+            
+            prev = curr;
+            curr = next;
+            if (curr != nullptr) {
+                next = curr->next;
+            }
+        }
+        
+        return prev;
+    }
+};
