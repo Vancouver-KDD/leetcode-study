fc6f68d59eb9588c49cf526a4938591b693891c2
juyomo <j.92.moon@gmail.com>
2025-07-22 21:14:13 +0900

    Week 8 solns

---
+++ 2025/JuyoungMoon/week8/2025-07-27-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,24 @@
+// Author: Juyoung Moon
+
+// KDD LeetCode Study Week 8: Heap / Priority Queue
+// https://github.com/juyomo/leetcode-study
+
+// LeetCode #215.
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        // priority_queue< int, vector<int>, less<int> > pq;
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push(nums[i]);
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+
+        return pq.top();
+    }
+};
---
+++ 2025/JuyoungMoon/week8/2025-07-27-task-scheduler.cpp
@@ -0,0 +1,8 @@
+// Author: Juyoung Moon
+
+// KDD LeetCode Study Week 8: Heap / Priority Queue
+// https://github.com/juyomo/leetcode-study
+
+// LeetCode #621.
+// https://leetcode.com/problems/task-scheduler/
+
