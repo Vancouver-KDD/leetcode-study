2ad130e525b8976425e255d375f0191ba4d01b7f
juyomo <juyomo@users.noreply.github.com>
2025-07-30 00:30:44 +0900

    Update 2025-07-27-task-scheduler.cpp

--- 2025/JuyoungMoon/week8/2025-07-27-task-scheduler.cpp
+++ 2025/JuyoungMoon/week8/2025-07-27-task-scheduler.cpp
@@ -6,3 +6,33 @@
 // LeetCode #621.
 // https://leetcode.com/problems/task-scheduler/
 
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char, int> counts;
+        for (char c : tasks) {
+            counts[c]++;
+        }
+
+        int maxFreq = 0;
+        int total = 0;
+        for (const auto& p : counts) {
+            if (p.second > maxFreq) {
+                maxFreq = p.second;
+            }
+            total += p.second;
+        }
+
+        int howManyAtMaxFreq = 0;
+        for (const auto& p : counts) {
+            if (p.second == maxFreq) {
+                howManyAtMaxFreq++;
+            }
+        }
+
+        int width = n + 1;
+        int res = width * (maxFreq - 1) + howManyAtMaxFreq;
+        
+        return max(total, res);
+    }
+};
