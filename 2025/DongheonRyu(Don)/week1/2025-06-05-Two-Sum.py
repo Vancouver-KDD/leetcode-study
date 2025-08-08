df318c6d36fcad48272152d71ece22f4f2466838
Don Ryu <alpharyu1031@gmail.com>
2025-08-07 21:07:36 -0700

    re-update

---
+++ 2025/DongheonRyu(Don)/week1/2025-06-05-Two-Sum.py
@@ -0,0 +1,27 @@
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+    # o(n^2)
+    
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if(j != i and nums[i]+nums[j] == target):
+                return [i,j]
+
+    return                 
+
+            
+    # o(n)
+def two_sum2(nums, target):
+    num_map = {}
+
+    for i in range(len(nums)):
+        
+        complement = target - nums[i]
+
+        if complement in num_map:
+            return [num_map[complement], i]
+
+        num_map[nums[i]] = i
+
+    return 
