df6f1f0c1baf337c3fbe4e376ae1716a3ad83d3c
scott0929 <scott0929@gmail.com>
2025-11-03 16:37:30 -0800

    JusungPark_week3

---
+++ 2025_Oct/JusungPark/2025-11-03-3Sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; 
+
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++; right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (sum < 0) left++;
+                else right--;
+            }
+        }
+        return result;
+    }
+}
