832d7e4f653c5dd7d6d3880ec63400d684c5d562
Kyle Cheon <kcheon@my.bcit.ca>
2025-11-15 13:43:44 -0800

    feat: add answer for first question

---
+++ 2025_Oct/Kyle/2025-11-15-Search-In-Rotated-Sorted-Array.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def search(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if target == nums[mid]:
+                return mid
+
+            # left sorted portion
+            if nums[left] <= nums[mid]:
+                if target > nums[mid] or target < nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            # right sorted portion
+            else:
+                if target < nums[mid] or target > nums[right]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+        return -1
