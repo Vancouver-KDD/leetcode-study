21cd56ec718ba8079fbf54ac0d1ff63f68466e3b
Kyounghan <dev.hanee.1@gmail.com>
2025-11-23 13:39:21 -0800

    102,105,236

--- 2025_Oct/Egloo/2025-11-24-[105]-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 2025_Oct/Egloo/2025-11-24-[105]-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,1 +1,45 @@
-'hello' 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def __init__(self):
+        self.index = 0
+        self.iMap = {}
+        self.preorder = []
+
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        self.index = 0
+        self.iMap = {}
+        self.preorder = preorder
+        
+        for idx, value in enumerate(inorder):
+            self.iMap[value] = idx
+
+        return self.build(0, len(inorder)-1)
+
+    def build(self, start, end):
+        if start > end:
+            return None
+        c_val = self.preorder[self.index]
+        current = self.iMap[c_val]
+
+        if not current in range(start, end+1):
+            return None
+
+        node = TreeNode(c_val)
+        self.index += 1
+
+        node.left = self.build(start, current-1)
+        node.right = self.build(current + 1, end)
+        
+        return node
+        
--- 2025_Oct/Egloo/2025-11-24-[236]-lowest-common-ancestor-of-binary-tree.py
+++ 2025_Oct/Egloo/2025-11-24-[236]-lowest-common-ancestor-of-binary-tree.py
@@ -1,1 +1,35 @@
-'hello' 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        answer = [None]
+        self.travel(root, p, q, answer)
+
+        return answer[0]
+
+
+    def travel(self, node, p, q, answer):
+        if not node:
+            return 0
+
+        l = self.travel(node.left, p, q, answer)
+        r = self.travel(node.right, p, q, answer)
+        
+        c = 1 if node in [p, q] else 0
+
+        r = l + r + c
+        if r == 2 and not answer[0]:
+            answer[0] = node
+
+        return  r
