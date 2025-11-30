15c5522d9b20822febfe46ed31325b6920f5b6f2
Anica1004 <anicamok@gmail.com>
2025-11-29 22:27:57 -0800

    Week 7

---
+++ 2025_Oct/Anica/week7/2025-12-01-combination-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def backtracking(i, path, summation):
+            if summation == target and i <= len(candidates):
+                res.append(path.copy())
+                return 
+            if i >= len(candidates) or summation > target: 
+                return 
+
+            else: 
+                path.append(candidates[i])
+                backtracking(i, path, summation+candidates[i])
+                path.pop()
+                backtracking(i+1, path, summation)
+
+        backtracking(0, [], 0)
+        return res
+
+
+
+
+
+
+        
